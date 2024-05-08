8f0acd681a33c945310393489a6a6edf582da2a4
Sebastien <byasebastien@hotmail.com>
2024-05-08 16:03:02 +0200

    CRUD Product

--- src/main/java/be/bstorm/tf_java2024_exomvc/domain/entities/User.java
+++ src/main/java/be/bstorm/tf_java2024_exomvc/domain/entities/User.java
@@ -2,10 +2,7 @@ package be.bstorm.tf_java2024_exomvc.domain.entities;
 
 import be.bstorm.tf_java2024_exomvc.domain.enums.UserRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/be/bstorm/tf_java2024_exomvc/pl/controllers/ProductController.java
+++ src/main/java/be/bstorm/tf_java2024_exomvc/pl/controllers/ProductController.java
@@ -2,12 +2,14 @@ package be.bstorm.tf_java2024_exomvc.pl.controllers;
 
 import be.bstorm.tf_java2024_exomvc.bll.services.ProductService;
 import be.bstorm.tf_java2024_exomvc.domain.entities.Product;
+import be.bstorm.tf_java2024_exomvc.pl.models.product.forms.ProductForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +32,61 @@ public class ProductController {
         Product product = productService.getProductById(id);
         model.addAttribute("product", product);
         return "product/details";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/create")
+    public String createProduct(Model model) {
+        model.addAttribute("form", new ProductForm());
+        return "product/create";
+    }
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PostMapping("/create")
+    public String createProduct(
+            @ModelAttribute(name = "form") @Valid ProductForm productForm,
+            BindingResult bindingResult,
+            Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("form", productForm);
+            return "product/create";
+        }
+        productService.addProduct(productForm.toEntity());
+        return "redirect:/product";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/update/{id:^[0-9]+$}")
+    public String updateProduct(
+            @PathVariable Long id,
+            Model model
+    ) {
+        ProductForm form = ProductForm.fromEntity(productService.getProductById(id));
+        model.addAttribute("form", form);
+        model.addAttribute("productId", id);
+        return "product/update";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PostMapping("/update/{id:^[0-9]+$}")
+    public String updateProduct(
+            @PathVariable Long id,
+            @ModelAttribute(name = "form") @Valid ProductForm form,
+            BindingResult bindingResult,
+            Model model
+    ){
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("form", form);
+            model.addAttribute("productId", id);
+            return "product/update";
+        }
+        productService.updateProduct(id, form.toEntity());
+        return "redirect:/product";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/delete/{id:^[0-9]+$}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/product";
     }
 }
